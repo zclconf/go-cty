4d99de202d506b326a00706bbb621d6b039b3cf0
Martin Atkins <mart@degeneration.co.uk>
2017-02-12 11:44:51 -0800

    Add ElementType to Type, as an alias for the collection type method

--- cty/collection.go
+++ cty/collection.go
@@ -1,5 +1,9 @@
 package cty
 
+import (
+	"errors"
+)
+
 type collectionTypeImpl interface {
 	ElementType() Type
 }
@@ -9,4 +13,14 @@ type collectionTypeImpl interface {
 func (t Type) IsCollectionType() bool {
 	_, ok := t.typeImpl.(collectionTypeImpl)
 	return ok
+}
+
+// ElementType returns the element type of the receiver if it is a collection
+// type, or panics if it is not. Use IsCollectionType first to test whether
+// this method will succeed.
+func (t Type) ElementType() Type {
+	if ct, ok := t.typeImpl.(collectionTypeImpl); ok {
+		return ct.ElementType()
+	}
+	panic(errors.New("not a collection type"))
 }
