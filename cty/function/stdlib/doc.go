bf0a8f28094dcb8e4d838cd4624e57112920d979
Martin Atkins <mart@degeneration.co.uk>
2017-05-04 07:31:14 -0700

    function/stdlib: establish package of "standard library" functions
    The idea with this is that applications building on cty ought to behave
    in a somewhat-consistent way with regard to certain fundamental functions,
    including consistent handling of unicode strings, big floats, etc. By
    centralizing these here it's more likely that their implementations will
    be common across multiple callers, particularly if these functions are
    used as part of the implementation of other functions.

---
+++ cty/function/stdlib/doc.go
@@ -0,0 +1,8 @@
+// Package stdlib is a collection of cty functions that are expected to be
+// generally useful, and are thus factored out into this shared library in
+// the hope that cty-using applications will have consistent behavior when
+// using these functions.
+//
+// See the parent package "function" for more information on the purpose
+// and usage of cty functions.
+package stdlib
