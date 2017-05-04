b9d7a2770370230acf584cb6ff0df9203f7d17d7
Martin Atkins <mart@degeneration.co.uk>
2017-05-04 07:57:28 -0700

    function/stdlib: Reverse function for strings

--- cty/function/stdlib/string.go
+++ cty/function/stdlib/string.go
@@ -3,6 +3,8 @@ package stdlib
 import (
 	"strings"
 
+	"golang.org/x/text/unicode/norm"
+
 	"github.com/apparentlymart/go-cty/cty"
 	"github.com/apparentlymart/go-cty/cty/function"
 )
@@ -38,5 +40,37 @@ var Lower = function.New(&function.Spec{
 		in := args[0].AsString()
 		out := strings.ToLower(in)
 		return cty.StringVal(out), nil
+	},
+})
+
+// Reverse is a Function that reverses the order of the characters in the
+// given string.
+//
+// As usual, "character" for the sake of this function is a grapheme cluster,
+// so combining diacritics (for example) will be considered together as a
+// single character.
+var Reverse = function.New(&function.Spec{
+	Params: []function.Parameter{
+		{
+			Name:             "str",
+			Type:             cty.String,
+			AllowDynamicType: true,
+		},
+	},
+	Type: function.StaticReturnType(cty.String),
+	Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
+		in := []byte(args[0].AsString())
+		out := make([]byte, len(in))
+		pos := len(out)
+
+		for i := 0; i < len(in); {
+			d := norm.NFC.NextBoundary(in[i:], true)
+			cluster := in[i : i+d]
+			pos -= len(cluster)
+			copy(out[pos:], cluster)
+			i += d
+		}
+
+		return cty.StringVal(string(out)), nil
 	},
 })
--- cty/function/stdlib/string_test.go
+++ cty/function/stdlib/string_test.go
@@ -125,3 +125,76 @@ func TestLower(t *testing.T) {
 		})
 	}
 }
+
+func TestReverse(t *testing.T) {
+	tests := []struct {
+		Input cty.Value
+		Want  cty.Value
+	}{
+		{
+			cty.StringVal("hello"),
+			cty.StringVal("olleh"),
+		},
+		{
+			cty.StringVal(""),
+			cty.StringVal(""),
+		},
+		{
+			cty.StringVal("1"),
+			cty.StringVal("1"),
+		},
+		{
+			cty.StringVal("Живой Журнал"),
+			cty.StringVal("ланруЖ йовиЖ"),
+		},
+		{
+			// note that the dieresis here is intentionally a combining
+			// ligature.
+			cty.StringVal("noël"),
+			cty.StringVal("lëon"),
+		},
+		{
+			// The Es in this string has three combining acute accents.
+			// This tests something that NFC-normalization cannot collapse
+			// into a single precombined codepoint, since otherwise we might
+			// be cheating and relying on the single-codepoint forms.
+			cty.StringVal("wé́́é́́é́́!"),
+			cty.StringVal("!é́́é́́é́́w"),
+		},
+		{
+			// Go's normalization forms don't handle this ligature, so we
+			// will produce the wrong result but this is now a compatibility
+			// constraint and so we'll test it.
+			cty.StringVal("baﬄe"),
+			cty.StringVal("eﬄab"),
+		},
+		{
+			cty.StringVal("😸😾"),
+			cty.StringVal("😾😸"),
+		},
+		{
+			cty.UnknownVal(cty.String),
+			cty.UnknownVal(cty.String),
+		},
+		{
+			cty.DynamicVal,
+			cty.UnknownVal(cty.String),
+		},
+	}
+
+	reverse := Reverse.Proxy()
+
+	for _, test := range tests {
+		t.Run(test.Input.GoString(), func(t *testing.T) {
+			got, err := reverse(test.Input)
+
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !got.RawEquals(test.Want) {
+				t.Errorf("wrong result\ngot:  %#v\nwant: %#v", got, test.Want)
+			}
+		})
+	}
+}
