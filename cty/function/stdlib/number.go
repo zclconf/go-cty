68fdc18ca25eb455789f77419b522c3e80093985
Martin Atkins <mart@degeneration.co.uk>
2017-05-05 08:18:28 -0700

    function/stdlib: "Int" function

--- cty/function/stdlib/number.go
+++ cty/function/stdlib/number.go
@@ -2,6 +2,7 @@ package stdlib
 
 import (
 	"fmt"
+	"math/big"
 
 	"github.com/apparentlymart/go-cty/cty"
 	"github.com/apparentlymart/go-cty/cty/function"
@@ -55,6 +56,26 @@ var MaxFunc = function.New(&function.Spec{
 	},
 })
 
+var IntFunc = function.New(&function.Spec{
+	Params: []function.Parameter{
+		{
+			Name:             "num",
+			Type:             cty.Number,
+			AllowDynamicType: true,
+		},
+	},
+	Type: function.StaticReturnType(cty.Number),
+	Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
+		bf := args[0].AsBigFloat()
+		if bf.IsInt() {
+			return args[0], nil
+		}
+		bi, _ := bf.Int(nil)
+		bf = (&big.Float{}).SetInt(bi)
+		return cty.NumberVal(bf), nil
+	},
+})
+
 // Min returns the minimum number from the given numbers.
 func Min(numbers ...cty.Value) (cty.Value, error) {
 	return MinFunc.Call(numbers)
@@ -63,4 +84,16 @@ func Min(numbers ...cty.Value) (cty.Value, error) {
 // Max returns the maximum number from the given numbers.
 func Max(numbers ...cty.Value) (cty.Value, error) {
 	return MaxFunc.Call(numbers)
+}
+
+// Int removes the fractional component of the given number returning an
+// integer representing the whole number component, rounding towards zero.
+// For example, -1.5 becomes -1.
+//
+// If an infinity is passed to Int, an error is returned.
+func Int(num cty.Value) (cty.Value, error) {
+	if num == cty.PositiveInfinity || num == cty.NegativeInfinity {
+		return cty.NilVal, fmt.Errorf("can't truncate infinity to an integer")
+	}
+	return IntFunc.Call([]cty.Value{num})
 }
--- cty/function/stdlib/number_test.go
+++ cty/function/stdlib/number_test.go
@@ -2,6 +2,7 @@ package stdlib
 
 import (
 	"fmt"
+	"math/big"
 	"testing"
 
 	"github.com/apparentlymart/go-cty/cty"
@@ -121,4 +122,70 @@ func TestMax(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestInt(t *testing.T) {
+	tests := []struct {
+		Input cty.Value
+		Want  cty.Value
+	}{
+		{
+			cty.NumberIntVal(0),
+			cty.NumberIntVal(0),
+		},
+		{
+			cty.NumberIntVal(1),
+			cty.NumberIntVal(1),
+		},
+		{
+			cty.NumberIntVal(-1),
+			cty.NumberIntVal(-1),
+		},
+		{
+			cty.NumberFloatVal(1.3),
+			cty.NumberIntVal(1),
+		},
+		{
+			cty.NumberFloatVal(-1.7),
+			cty.NumberIntVal(-1),
+		},
+		{
+			cty.NumberFloatVal(-1.3),
+			cty.NumberIntVal(-1),
+		},
+		{
+			cty.NumberFloatVal(-1.7),
+			cty.NumberIntVal(-1),
+		},
+		{
+			cty.NumberVal(mustParseFloat("999999999999999999999999999999999999999999999999999999999999.7")),
+			cty.NumberVal(mustParseFloat("999999999999999999999999999999999999999999999999999999999999")),
+		},
+		{
+			cty.NumberVal(mustParseFloat("-999999999999999999999999999999999999999999999999999999999999.7")),
+			cty.NumberVal(mustParseFloat("-999999999999999999999999999999999999999999999999999999999999")),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Input.GoString(), func(t *testing.T) {
+			got, err := Int(test.Input)
+
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !got.RawEquals(test.Want) {
+				t.Errorf("wrong result\ngot:  %#v\nwant: %#v", got, test.Want)
+			}
+		})
+	}
+}
+
+func mustParseFloat(s string) *big.Float {
+	ret, _, err := big.ParseFloat(s, 10, 0, big.AwayFromZero)
+	if err != nil {
+		panic(err)
+	}
+	return ret
 }
