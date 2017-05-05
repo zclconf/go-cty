3bcc7fd80c943d3a200f7a7e28a405fb064c1de7
Martin Atkins <mart@degeneration.co.uk>
2017-05-05 07:57:11 -0700

    function/stdlib: "Min" function

--- cty/function/stdlib/number.go
+++ cty/function/stdlib/number.go
@@ -7,6 +7,30 @@ import (
 	"github.com/apparentlymart/go-cty/cty/function"
 )
 
+var MinFunc = function.New(&function.Spec{
+	Params: []function.Parameter{},
+	VarParam: &function.Parameter{
+		Name:             "numbers",
+		Type:             cty.Number,
+		AllowDynamicType: true,
+	},
+	Type: function.StaticReturnType(cty.Number),
+	Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
+		if len(args) == 0 {
+			return cty.NilVal, fmt.Errorf("must pass at least one number")
+		}
+
+		min := cty.PositiveInfinity
+		for _, num := range args {
+			if num.LessThan(min).True() {
+				min = num
+			}
+		}
+
+		return min, nil
+	},
+})
+
 var MaxFunc = function.New(&function.Spec{
 	Params: []function.Parameter{},
 	VarParam: &function.Parameter{
@@ -30,6 +54,11 @@ var MaxFunc = function.New(&function.Spec{
 		return max, nil
 	},
 })
+
+// Min returns the minimum number from the given numbers.
+func Min(numbers ...cty.Value) (cty.Value, error) {
+	return MinFunc.Call(numbers)
+}
 
 // Max returns the maximum number from the given numbers.
 func Max(numbers ...cty.Value) (cty.Value, error) {
--- cty/function/stdlib/number_test.go
+++ cty/function/stdlib/number_test.go
@@ -7,6 +7,64 @@ import (
 	"github.com/apparentlymart/go-cty/cty"
 )
 
+func TestMin(t *testing.T) {
+	tests := []struct {
+		Inputs []cty.Value
+		Want   cty.Value
+	}{
+		{
+			[]cty.Value{cty.NumberIntVal(0)},
+			cty.NumberIntVal(0),
+		},
+		{
+			[]cty.Value{cty.NumberIntVal(-12)},
+			cty.NumberIntVal(-12),
+		},
+		{
+			[]cty.Value{cty.NumberIntVal(12)},
+			cty.NumberIntVal(12),
+		},
+		{
+			[]cty.Value{cty.NumberIntVal(-12), cty.NumberIntVal(0), cty.NumberIntVal(2)},
+			cty.NumberIntVal(-12),
+		},
+		{
+			[]cty.Value{cty.NegativeInfinity, cty.NumberIntVal(0)},
+			cty.NegativeInfinity,
+		},
+		{
+			[]cty.Value{cty.PositiveInfinity, cty.NumberIntVal(0)},
+			cty.NumberIntVal(0),
+		},
+		{
+			[]cty.Value{cty.NegativeInfinity},
+			cty.NegativeInfinity,
+		},
+		{
+			[]cty.Value{cty.PositiveInfinity, cty.UnknownVal(cty.Number)},
+			cty.UnknownVal(cty.Number),
+		},
+		{
+			[]cty.Value{cty.PositiveInfinity, cty.DynamicVal},
+			cty.UnknownVal(cty.Number),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%#v", test.Inputs), func(t *testing.T) {
+			got, err := Min(test.Inputs...)
+
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !got.RawEquals(test.Want) {
+				t.Errorf("wrong result\ngot:  %#v\nwant: %#v", got, test.Want)
+			}
+		})
+	}
+}
+
 func TestMax(t *testing.T) {
 	tests := []struct {
 		Inputs []cty.Value
