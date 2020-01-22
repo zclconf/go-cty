76240aa3c2df7e753d5dda7908c865af9e91bc2e
Adrien Delorme <adrien.delorme@icloud.com>
2020-01-22 12:59:28 +0100

    Remove MakeFileFunc for now
    Quoting our discussion: https://github.com/zclconf/go-cty/pull/37#discussion_r369349174
    
    This one doesn't feel like a good fit for cty stdlib because it makes some assumptions about how we deal with the filesystem that are unlikely to be a good fit for all callers. If this were to be in cty we'd want to think about ways to let the calling application have more control over how filenames are resolved so that e.g. it could be safe to use the filesystem-related functions in an app with different security concerns, but that would likely make it a lot more complicated. So this will be done in another PR.

--- cty/function/stdlib/filesystem.go
+++ cty/function/stdlib/filesystem.go
@@ -1,51 +1,16 @@
 package stdlib
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"unicode/utf8"
 
 	"github.com/bmatcuk/doublestar"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
 )
-
-// MakeFileFunc is a function that takes a file path and returns the
-// contents of that file, either directly as a string (where valid UTF-8 is
-// required) or as a string containing base64 bytes.
-func MakeFileFunc(baseDir string, encBase64 bool) function.Function {
-	return function.New(&function.Spec{
-		Params: []function.Parameter{
-			{
-				Name: "path",
-				Type: cty.String,
-			},
-		},
-		Type: function.StaticReturnType(cty.String),
-		Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
-			path := args[0].AsString()
-			src, err := readFileBytes(baseDir, path)
-			if err != nil {
-				return cty.UnknownVal(cty.String), err
-			}
-
-			switch {
-			case encBase64:
-				enc := base64.StdEncoding.EncodeToString(src)
-				return cty.StringVal(enc), nil
-			default:
-				if !utf8.Valid(src) {
-					return cty.UnknownVal(cty.String), fmt.Errorf("contents of %s are not valid UTF-8; use the filebase64 function to obtain the Base64 encoded contents or the other file functions (e.g. filemd5, filesha256) to obtain file hashing results instead", path)
-				}
-				return cty.StringVal(string(src)), nil
-			}
-		},
-	})
-}
 
 // MakeFileExistsFunc is a function that takes a path
 // and determines whether a file exists at that path
@@ -245,18 +210,6 @@ func readFileBytes(baseDir, path string) ([]byte, error) {
 	return src, nil
 }
 
-// File reads the contents of the file at the given path.
-//
-// The file must contain valid UTF-8 bytes, or this function will return an error.
-//
-// The underlying function implementation works relative to a particular base
-// directory, so this wrapper takes a base directory string and uses it to
-// construct the underlying function before calling it.
-func File(baseDir string, path cty.Value) (cty.Value, error) {
-	fn := MakeFileFunc(baseDir, false)
-	return fn.Call([]cty.Value{path})
-}
-
 // FileExists determines whether a file exists at the given path.
 //
 // The underlying function implementation works relative to a particular base
@@ -275,18 +228,6 @@ func FileExists(baseDir string, path cty.Value) (cty.Value, error) {
 func FileSet(baseDir string, path, pattern cty.Value) (cty.Value, error) {
 	fn := MakeFileSetFunc(baseDir)
 	return fn.Call([]cty.Value{path, pattern})
-}
-
-// FileBase64 reads the contents of the file at the given path.
-//
-// The bytes from the file are encoded as base64 before returning.
-//
-// The underlying function implementation works relative to a particular base
-// directory, so this wrapper takes a base directory string and uses it to
-// construct the underlying function before calling it.
-func FileBase64(baseDir string, path cty.Value) (cty.Value, error) {
-	fn := MakeFileFunc(baseDir, true)
-	return fn.Call([]cty.Value{path})
 }
 
 // Basename takes a string containing a filesystem path and removes all except the last portion from it.
