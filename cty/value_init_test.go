139e7944e764b5aa82226d61b7608d4e48b321ef
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2021-03-09 14:32:29 -0500

    convert: Don't panic when collection element type unification fails
    Despite our best efforts to correctly unify element types during conversion,
    sometimes some inconsistent types slip through anyway and make the
    conversion panic.
    
    Although in the long run it would be better to fix these problems upstream,
    it's the responsibility of the caller of cty to not try to perform any
    invalid operations, and so here we make the convert package pre-verify
    that it's found a sensible result before trying to construct it, and
    thus it can return a proper error if not rather than panicking.

--- cty/value_init_test.go
+++ cty/value_init_test.go
@@ -116,3 +116,319 @@ func TestSetVal_nestedStructures(t *testing.T) {
 		})
 	}
 }
+
+func TestCanListVal(t *testing.T) {
+	testCases := []struct {
+		Elems []Value
+		Want  bool
+	}{
+		// Valid lists
+		{
+			[]Value{StringVal("Hello"), StringVal("World")},
+			true,
+		},
+		{
+			[]Value{NumberIntVal(13), NumberIntVal(31)},
+			true,
+		},
+		{
+			[]Value{BoolVal(true), BoolVal(false)},
+			true,
+		},
+		{
+			[]Value{
+				ListVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				ListVal([]Value{
+					StringVal("beep"), StringVal("boop"), StringVal("bloop"),
+				}),
+			},
+			true,
+		},
+		{
+			[]Value{
+				MapVal(map[string]Value{
+					"a": StringVal("Hello"),
+				}),
+				MapVal(map[string]Value{
+					"c": StringVal("World"),
+				}),
+			},
+			true,
+		},
+		{
+			[]Value{
+				SetVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				SetVal([]Value{
+					StringVal("beep"), StringVal("boop"), StringVal("bloop"),
+				}),
+			},
+			true,
+		},
+		// invalid list elements
+		{
+			[]Value{StringVal("hello"), NumberIntVal(13)},
+			false,
+		},
+		{
+			[]Value{
+				ListVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				MapVal(map[string]Value{
+					"a": StringVal("bloop"),
+				}),
+			},
+			false,
+		},
+		{ // List of string and List of lists
+			[]Value{
+				ListVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				ListVal([]Value{
+					ListVal([]Value{
+						StringVal("a"), StringVal("b"),
+					}),
+					ListVal([]Value{
+						StringVal("c"), StringVal("d"),
+					}),
+				}),
+			},
+			false,
+		},
+		{ // Inconsistent map elements
+			[]Value{
+				MapVal(map[string]Value{
+					"a": StringVal("Hello"),
+				}),
+				MapVal(map[string]Value{
+					"a": BoolVal(true),
+				}),
+			},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		got := CanListVal(tc.Elems)
+		if got != tc.Want {
+			t.Errorf("wrong result for elements %#v:\ngot %v, want %v", tc.Elems, got, tc.Want)
+		}
+	}
+}
+
+func TestCanSetVal(t *testing.T) {
+	testCases := []struct {
+		Elems []Value
+		Want  bool
+	}{
+		// Valid set elements
+		{
+			[]Value{StringVal("Hello"), StringVal("World")},
+			true,
+		},
+		{
+			[]Value{StringVal("Hello").Mark(1), StringVal("World").Mark(2)},
+			true,
+		},
+		{
+			[]Value{NumberIntVal(13), NumberIntVal(31)},
+			true,
+		},
+		{
+			[]Value{BoolVal(true), BoolVal(false)},
+			true,
+		},
+		{
+			[]Value{
+				ListVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				ListVal([]Value{
+					StringVal("beep"), StringVal("boop"), StringVal("bloop"),
+				}),
+			},
+			true,
+		},
+		{
+			[]Value{
+				MapVal(map[string]Value{
+					"a": StringVal("Hello"),
+				}),
+				MapVal(map[string]Value{
+					"c": StringVal("World"),
+				}),
+			},
+			true,
+		},
+		{
+			[]Value{
+				SetVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				SetVal([]Value{
+					StringVal("beep"), StringVal("boop"), StringVal("bloop"),
+				}),
+			},
+			true,
+		},
+		// invalid set elements
+		{
+			[]Value{StringVal("hello"), NumberIntVal(13)},
+			false,
+		},
+		{
+			[]Value{
+				ListVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				MapVal(map[string]Value{
+					"a": StringVal("bloop"),
+				}),
+			},
+			false,
+		},
+		{ // List of string and List of lists
+			[]Value{
+				ListVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				ListVal([]Value{
+					ListVal([]Value{
+						StringVal("a"), StringVal("b"),
+					}),
+					ListVal([]Value{
+						StringVal("c"), StringVal("d"),
+					}),
+				}),
+			},
+			false,
+		},
+		{ // Inconsistent map elements
+			[]Value{
+				MapVal(map[string]Value{
+					"a": StringVal("Hello"),
+				}),
+				MapVal(map[string]Value{
+					"a": BoolVal(true),
+				}),
+			},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		got := CanSetVal(tc.Elems)
+		if got != tc.Want {
+			t.Errorf("wrong result for elements %#v:\ngot %v, want %v", tc.Elems, got, tc.Want)
+		}
+	}
+}
+
+func TestCanMapVal(t *testing.T) {
+	testCases := []struct {
+		Elems map[string]Value
+		Want  bool
+	}{
+		// Valid lists
+		{
+			map[string]Value{"a": StringVal("Hello"), "b": StringVal("World")},
+			true,
+		},
+		{
+			map[string]Value{"one": NumberIntVal(13), "two": NumberIntVal(31)},
+			true,
+		},
+		{
+			map[string]Value{"one": BoolVal(true), "two": BoolVal(false)},
+			true,
+		},
+		{
+			map[string]Value{
+				"lista": ListVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				"listb": ListVal([]Value{
+					StringVal("beep"), StringVal("boop"), StringVal("bloop"),
+				}),
+			},
+			true,
+		},
+		{
+			map[string]Value{
+				"map_a": MapVal(map[string]Value{
+					"a": StringVal("Hello"),
+				}),
+				"map_b": MapVal(map[string]Value{
+					"c": StringVal("World"),
+				}),
+			},
+			true,
+		},
+		{
+			map[string]Value{
+				"set_a": SetVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				"set_b": SetVal([]Value{
+					StringVal("beep"), StringVal("boop"), StringVal("bloop"),
+				}),
+			},
+			true,
+		},
+		// invalid map elements
+		{
+			map[string]Value{"one": StringVal("hello"), "two": NumberIntVal(13)},
+			false,
+		},
+		{
+			map[string]Value{
+				"one": ListVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				"two": MapVal(map[string]Value{
+					"a": StringVal("bloop"),
+				}),
+			},
+			false,
+		},
+		{
+			map[string]Value{
+				"one": ListVal([]Value{
+					StringVal("Hello"), StringVal("World"),
+				}),
+				"two": ListVal([]Value{
+					ListVal([]Value{
+						StringVal("a"), StringVal("b"),
+					}),
+					ListVal([]Value{
+						StringVal("c"), StringVal("d"),
+					}),
+				}),
+			},
+			false,
+		},
+		{ // Inconsistent map elements
+			map[string]Value{
+				"one": MapVal(map[string]Value{
+					"a": StringVal("Hello"),
+				}),
+				"two": MapVal(map[string]Value{
+					"a": BoolVal(true),
+				}),
+			},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		got := CanMapVal(tc.Elems)
+		if got != tc.Want {
+			t.Errorf("wrong result for elements %#v:\ngot %v, want %v", tc.Elems, got, tc.Want)
+		}
+	}
+}
