90fd0d5205efb4de4b730cde55ac8f4cbdc12f02
James Bardin <j.bardin@gmail.com>
2021-03-09 19:32:45 -0500

    convert: unify mixtures similar of structural and collection types
    We already had special behavior to deal with unifying lists of
    incompatible element types and tuples of incompatible
    types, but we didn't have a similar special case for unifying
    a mixture of list and tuple types and so we were falling back
    on the default "search for an available conversion" behavior
    and getting a different result in that scenario compared to
    how we handle unification of just different list types.
    
    This situation also applies to mixtures of map and object
    types, so this change adds two new special cases to deal
    with both of those situations.

--- cty/convert/unify_test.go
+++ cty/convert/unify_test.go
@@ -143,6 +143,321 @@ func TestUnify(t *testing.T) {
 			nil,
 		},
 		{
+			// objects can unify as map(string) within the tuples
+			[]cty.Type{
+				cty.Tuple([]cty.Type{
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+				}),
+				cty.Tuple([]cty.Type{
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.String,
+					}),
+				}),
+			},
+			cty.List(cty.Map(cty.String)),
+			[]bool{true, true},
+		},
+		{
+			// unifies to the same result as above, since the only difference
+			// is the addition of a list
+			[]cty.Type{
+				cty.List(cty.Object(map[string]cty.Type{
+					"a": cty.String,
+				})),
+				cty.Tuple([]cty.Type{
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.String,
+					}),
+				}),
+				cty.Tuple([]cty.Type{
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.String,
+					}),
+					cty.Object(map[string]cty.Type{
+						"c": cty.String,
+						"d": cty.String,
+					}),
+				}),
+			},
+			cty.List(cty.Map(cty.String)),
+			[]bool{true, true, true},
+		},
+		{
+			// Ensure the map does not change the unification process
+			[]cty.Type{
+				cty.List(cty.Object(map[string]cty.Type{
+					"a": cty.String,
+				})),
+				cty.List(cty.Map(cty.String)),
+				cty.Tuple([]cty.Type{
+					cty.Map(cty.String),
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.String,
+					}),
+				}),
+			},
+			cty.List(cty.Map(cty.String)),
+			[]bool{true, false, true},
+		},
+		{
+			// different tuple lengths unify as a list, and the objects can
+			// unify as maps
+			[]cty.Type{
+				cty.Tuple([]cty.Type{
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.Number,
+					}),
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.Number,
+					}),
+				}),
+				cty.Tuple([]cty.Type{
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+				}),
+			},
+			cty.List(cty.Map(cty.String)),
+			[]bool{true, true},
+		},
+		{
+			// the equivalent tuple lengths still unify as a tuple, though the
+			// objects are unified as a map
+			[]cty.Type{
+				cty.Tuple([]cty.Type{
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.Number,
+					}),
+				}),
+				cty.Tuple([]cty.Type{
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+				}),
+			},
+			cty.Tuple([]cty.Type{cty.Map(cty.String)}),
+			[]bool{true, true},
+		},
+		{
+			// This should unify to like the tuple above
+			[]cty.Type{
+				cty.List(
+					cty.Object(map[string]cty.Type{
+						"a": cty.Number,
+						"b": cty.String,
+					}),
+				),
+				cty.Tuple([]cty.Type{
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+				}),
+			},
+			cty.List(cty.Map(cty.String)),
+			[]bool{true, true},
+		},
+		{
+			// This should also unify like the previous 2 examples
+			[]cty.Type{
+				cty.List(
+					cty.Object(map[string]cty.Type{
+						"a": cty.Number,
+						"b": cty.String,
+					}),
+				),
+				cty.List(cty.Object(map[string]cty.Type{
+					"a": cty.String,
+				})),
+			},
+			cty.List(cty.Map(cty.String)),
+			[]bool{true, true},
+		},
+		{
+			// Objects and maps should unify along with the surrounding lists
+			// and tuples.
+			[]cty.Type{
+				cty.List(cty.Object(map[string]cty.Type{
+					"a": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+					"b": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.String,
+					}),
+				})),
+				cty.List(cty.Map(
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.String,
+					}),
+				)),
+			},
+			cty.List(cty.Map(cty.Map(cty.String))),
+			[]bool{true, true},
+		},
+		{
+			// objects can unify as maps within objects
+			[]cty.Type{
+				cty.Object(map[string]cty.Type{
+					"a": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+				}),
+				cty.Object(map[string]cty.Type{
+					"a": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.String,
+					}),
+				}),
+			},
+			cty.Object(map[string]cty.Type{
+				"a": cty.Map(cty.String),
+			}),
+			[]bool{true, true},
+		},
+		{
+			// nested objects can unify as maps
+			[]cty.Type{
+				cty.Object(map[string]cty.Type{
+					"a": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+					"b": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.String,
+					}),
+				}),
+				cty.Map(
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+						"b": cty.String,
+					}),
+				),
+			},
+			cty.Map(cty.Map(cty.String)),
+			[]bool{true, true},
+		},
+		{
+			// nested tuples and lists can unify along with the surrounding
+			// objects and maps
+			[]cty.Type{
+				cty.Object(map[string]cty.Type{
+					"a": cty.Object(map[string]cty.Type{
+						"a": cty.List(cty.String),
+					}),
+					"b": cty.Object(map[string]cty.Type{
+						"a": cty.Tuple([]cty.Type{
+							cty.String,
+						}),
+						"b": cty.List(cty.String),
+					}),
+				}),
+				cty.Map(
+					cty.Object(map[string]cty.Type{
+						"a": cty.List(cty.String),
+						"b": cty.List(cty.String),
+					}),
+				),
+			},
+			cty.Map(cty.Map(cty.List(cty.String))),
+			[]bool{true, true},
+		},
+		{
+			// objects can unify as maps containing objects when all attributes
+			// match
+			[]cty.Type{
+				cty.Object(map[string]cty.Type{
+					"a": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+					"b": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+				}),
+				cty.Map(
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+				),
+			},
+			cty.Map(
+				cty.Object(map[string]cty.Type{
+					"a": cty.String,
+				}),
+			),
+			[]bool{true, false},
+		},
+		{
+			// objects can unify as maps with dynamic types
+			[]cty.Type{
+				cty.Object(map[string]cty.Type{
+					"a": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+					"b": cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+				}),
+				cty.Map(cty.DynamicPseudoType),
+				cty.Map(
+					cty.Object(map[string]cty.Type{
+						"a": cty.String,
+					}),
+				),
+			},
+			cty.Map(cty.DynamicPseudoType),
+			[]bool{true, false, true},
+		},
+		{
+			// deeply nested objects and maps can unify
+			[]cty.Type{
+				cty.Object(map[string]cty.Type{
+					"a": cty.Object(map[string]cty.Type{
+						"a": cty.Object(map[string]cty.Type{
+							"a": cty.String,
+						}),
+					}),
+					"b": cty.Object(map[string]cty.Type{
+						"c": cty.Object(map[string]cty.Type{
+							"d": cty.String,
+						}),
+					}),
+				}),
+				cty.Map(cty.Map(cty.Map(cty.String))),
+			},
+			cty.Map(cty.Map(cty.Map(cty.String))),
+			[]bool{true, false},
+		},
+		{
+			// deeply nested objects with maps can unify as maps
+			[]cty.Type{
+				cty.Map(cty.Map(cty.Map(cty.String))),
+				cty.Object(map[string]cty.Type{
+					"a": cty.Object(map[string]cty.Type{
+						"a": cty.Object(map[string]cty.Type{
+							"a": cty.String,
+						}),
+						"b": cty.Map(cty.String),
+					}),
+					"b": cty.Map(cty.Map(cty.String)),
+				}),
+			},
+			cty.Map(cty.Map(cty.Map(cty.String))),
+			[]bool{false, true},
+		},
+		{
 			[]cty.Type{
 				cty.DynamicPseudoType,
 				cty.Tuple([]cty.Type{cty.Number}),
