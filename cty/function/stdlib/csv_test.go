47a79612085e043d528fab49f8687ec6bd6a77e5
Martin Atkins <mart@degeneration.co.uk>
2021-06-28 14:22:53 -0700

    function/stdlib: Consistent csvdecode error messages
    Go 1.17 changes the way the CSV parser reports columns in its error
    objects, causing our error results to be inconsistent depending on which
    Go version we're built with.
    
    In order to make the error messages consistent we'll use our own custom
    error formatter for that case. This does mean we lose the column number
    in the error message, but we'll still include the line number where the
    problem was detected.

--- cty/function/stdlib/csv_test.go
+++ cty/function/stdlib/csv_test.go
@@ -58,7 +58,7 @@ func TestCSVDecode(t *testing.T) {
 		{
 			cty.StringVal(`invalid"thing"`),
 			cty.DynamicVal,
-			`parse error on line 1, column 7: bare " in non-quoted-field`,
+			`CSV parse error on line 1: bare " in non-quoted-field`,
 		},
 		{
 			cty.UnknownVal(cty.String),
