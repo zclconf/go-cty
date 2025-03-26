ca64fed5e3fdf82c9d7c49ddf811232045d34e91
Baraa Basata <bbasata@gmail.com>
2025-03-26 08:45:08 -0400

    Forward to 1.7.0 (#17)
    * Prepare for a potential future 1.6.1 release
    
    * build: remove go 1.11 and 1.12 from travis config
    
    * cty: RawEquals correct behavior for sets with partially-unknown values
    
    Sets with the same type and same unknown values are equal enough for
    RawEqual's purposes. This commit takes advantage of the setRules's
    defined ordering to compare each element of the sets as lists.
    
    * v1.6.1
    
    * Prepare for an assumed future v1.6.2 release
    
    * cty: Utilities to unmark with marks at specific paths
    
    This allows a caller to call a method like Unmark, but instead of getting a superset of marks, get an array of marks and their associated paths. This allows for a Value to be unmarked and then remarked later without losing so much detail about the marks.
    
    * Update CHANGELOG.md
    
    * cty: Fix deep marks functions with new transformer
    
    The UnmarkDeep, UnmarkDeepWithPaths, and MarkWithPaths functions could
    previously panic with nested complex values. This was due to the
    Transform callback function only being called as part of a postorder
    traversal, when it is not permitted to traverse a marked value.
    
    This commit introduces a new TransformWithTransformer function,
    requiring an implementation of a new Transformer interface. Using this
    interface, callers can implement either preorder or postorder
    traversals.
    
    In turn, this allows us to write deep transformation for marks which
    successfully cope with all nested structures.
    
    The commit includes significantly more tests for these functions, which
    should now cover all complex value types.
    
    * cty: Remove already-fixed FIXME for SetVal
    
    The immediately preceding code already unmarks the values and applies
    the marking to the set, so this comment and panic are out of date.
    
    * functions/stdlib: formatdate to correctly handle literals at the end of the format string
    
    * Update CHANGELOG.md
    
    * cty: Fix path array reuse with UnmarkDeepWithPaths
    
    When unmarking a complex value and retaining the marked paths, a
    sufficiently deep structure would result in incorrect duplicate path
    output. For example, this structure (in HCL syntax):
    
    {
      environment = [
        {
          variables = {
            "x" = 1
            "y" = 2
          }
        }
      ]
    }
    
    If the 1 and 2 values are marked, the resulting path value marks from
    UnmarkDeepWithPaths would have two entries, as expected. However, both
    would have the same Path attribute, like so:
    
    [
      { environment[0].variables["x"], "mark" },
      { environment[0].variables["x"], "mark" },
    ]
    
    This is caused by calling `append` in the walk transform function with
    the same path object repeatedly, which eventually does not result in
    array reallocation, and therefore the path object is modified in-place.
    
    * stdlib: Add tests for join function
    
    * function: Deeply unmark function arguments
    
    For functions which do not explicitly support marks, we now deeply
    unmark arguments before calling, and reapply those marks to the return
    value. This ensures that these functions do not panic with arguments
    which are collection values with marked descendants.
    
    This does result in overly conservatively applying marks in some cases,
    but that can be fixed with later work to add explicit support for marks
    to those functions.
    
    * Update CHANGELOG.md
    
    * v1.7.0
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Martin Atkins <mart@degeneration.co.uk>
    Co-authored-by: Kristin Laemmert <mildwonkey@users.noreply.github.com>
    Co-authored-by: Pam Selle <pamela.selle@gmail.com>
    Co-authored-by: Alisdair McDiarmid <alisdair@users.noreply.github.com>
    Co-authored-by: OwenTuz <owentuz@gmail.com>

--- cty/function/stdlib/string_test.go
+++ cty/function/stdlib/string_test.go
@@ -393,3 +393,83 @@ func TestSubstr(t *testing.T) {
 		})
 	}
 }
+
+func TestJoin(t *testing.T) {
+	tests := map[string]struct {
+		Separator cty.Value
+		Lists     []cty.Value
+		Want      cty.Value
+	}{
+		"single two-element list": {
+			cty.StringVal("-"),
+			[]cty.Value{
+				cty.ListVal([]cty.Value{cty.StringVal("hello"), cty.StringVal("world")}),
+			},
+			cty.StringVal("hello-world"),
+		},
+		"multiple single-element lists": {
+			cty.StringVal("-"),
+			[]cty.Value{
+				cty.ListVal([]cty.Value{cty.StringVal("chicken")}),
+				cty.ListVal([]cty.Value{cty.StringVal("egg")}),
+			},
+			cty.StringVal("chicken-egg"),
+		},
+		"single single-element list": {
+			cty.StringVal("-"),
+			[]cty.Value{
+				cty.ListVal([]cty.Value{cty.StringVal("chicken")}),
+			},
+			cty.StringVal("chicken"),
+		},
+		"blank separator": {
+			cty.StringVal(""),
+			[]cty.Value{
+				cty.ListVal([]cty.Value{cty.StringVal("horse"), cty.StringVal("face")}),
+			},
+			cty.StringVal("horseface"),
+		},
+		"marked list": {
+			cty.StringVal("-"),
+			[]cty.Value{
+				cty.ListVal([]cty.Value{cty.StringVal("hello"), cty.StringVal("world")}).Mark("sensitive"),
+			},
+			cty.StringVal("hello-world").Mark("sensitive"),
+		},
+		"marked separator": {
+			cty.StringVal("-").Mark("sensitive"),
+			[]cty.Value{
+				cty.ListVal([]cty.Value{cty.StringVal("hello"), cty.StringVal("world")}),
+			},
+			cty.StringVal("hello-world").Mark("sensitive"),
+		},
+		"list with some marked elements": {
+			cty.StringVal("-"),
+			[]cty.Value{
+				cty.ListVal([]cty.Value{cty.StringVal("hello").Mark("sensitive"), cty.StringVal("world")}),
+			},
+			cty.StringVal("hello-world").Mark("sensitive"),
+		},
+		"multiple marks": {
+			cty.StringVal("-").Mark("a"),
+			[]cty.Value{
+				cty.ListVal([]cty.Value{cty.StringVal("hello").Mark("b"), cty.StringVal("world").Mark("c")}),
+			},
+			cty.StringVal("hello-world").WithMarks(cty.NewValueMarks("a", "b", "c")),
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := Join(test.Separator, test.Lists...)
+
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !got.RawEquals(test.Want) {
+				t.Errorf("wrong result\ngot:  %#v\nwant: %#v", got, test.Want)
+			}
+		})
+	}
+}
--- cty/marks_test.go
+++ cty/marks_test.go
@@ -1,8 +1,104 @@
 package cty
 
 import (
+	"fmt"
 	"testing"
 )
+
+func TestContainsMarked(t *testing.T) {
+	testCases := []struct {
+		val  Value
+		want bool
+	}{
+		{
+			StringVal("a"),
+			false,
+		},
+		{
+			NumberIntVal(1).Mark("a"),
+			true,
+		},
+		{
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			false,
+		},
+		{
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2).Mark("a")}),
+			true,
+		},
+		{
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}).Mark("a"),
+			true,
+		},
+		{
+			ListValEmpty(String).Mark("c"),
+			true,
+		},
+		{
+			MapVal(map[string]Value{"a": StringVal("b").Mark("c"), "x": StringVal("y").Mark("z")}),
+			true,
+		},
+		{
+			TupleVal([]Value{NumberIntVal(1).Mark("a"), StringVal("y").Mark("z")}),
+			true,
+		},
+		{
+			SetVal([]Value{NumberIntVal(1).Mark("a"), NumberIntVal(2).Mark("z")}),
+			true,
+		},
+		{
+			ObjectVal(map[string]Value{
+				"x": ListVal([]Value{
+					NumberIntVal(1).Mark("a"),
+					NumberIntVal(2),
+				}),
+				"y": StringVal("y"),
+				"z": BoolVal(true),
+			}),
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		if got, want := tc.val.ContainsMarked(), tc.want; got != want {
+			t.Errorf("wrong result (got %v, want %v) for %#v", got, want, tc.val)
+		}
+	}
+}
+
+func TestIsMarked(t *testing.T) {
+	testCases := []struct {
+		val  Value
+		want bool
+	}{
+		{
+			StringVal("a"),
+			false,
+		},
+		{
+			NumberIntVal(1).Mark("a"),
+			true,
+		},
+		{
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			false,
+		},
+		{
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2).Mark("a")}),
+			false,
+		},
+		{
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}).Mark("a"),
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		if got, want := tc.val.IsMarked(), tc.want; got != want {
+			t.Errorf("wrong result (got %v, want %v) for %#v", got, want, tc.val)
+		}
+	}
+}
 
 func TestValueMarks(t *testing.T) {
 	v := True
@@ -60,5 +156,327 @@ func TestValueMarks(t *testing.T) {
 	result := a.Multiply(b).Subtract(c).GreaterThanOrEqualTo(d)
 	if got, want := result, False.WithMarks(NewValueMarks("a", "b", "c", "d")); !want.RawEquals(got) {
 		t.Errorf("wrong result\ngot:  %#v\nwant: %#v", got, want)
+	}
+
+	// Unmark the result and capture the paths
+	unmarkedResult, pvm := result.UnmarkDeepWithPaths()
+	// Remark the result with those paths
+	remarked := unmarkedResult.MarkWithPaths(pvm)
+	if got, want := remarked, False.WithMarks(NewValueMarks("a", "b", "c", "d")); !want.RawEquals(got) {
+		t.Errorf("wrong result\ngot:  %#v\nwant: %#v", got, want)
+	}
+
+	// If we call MarkWithPaths without any matching paths, we should get the unmarked result
+	markedWithNoPaths := unmarkedResult.MarkWithPaths([]PathValueMarks{{Path{IndexStep{Key: NumberIntVal(0)}}, NewValueMarks("z")}})
+	if got, want := markedWithNoPaths, False; !want.RawEquals(got) {
+		t.Errorf("wrong result\ngot:  %#v\nwant: %#v", got, want)
+	}
+}
+
+func TestPathValueMarksEqual(t *testing.T) {
+	tests := []struct {
+		original PathValueMarks
+		compare  PathValueMarks
+		want     bool
+	}{
+		{
+			PathValueMarks{Path{IndexStep{Key: NumberIntVal(0)}}, NewValueMarks("a")},
+			PathValueMarks{Path{IndexStep{Key: NumberIntVal(0)}}, NewValueMarks("a")},
+			true,
+		},
+		{
+			PathValueMarks{Path{IndexStep{Key: StringVal("p")}}, NewValueMarks(123)},
+			PathValueMarks{Path{IndexStep{Key: StringVal("p")}}, NewValueMarks(123)},
+			true,
+		},
+		{
+			PathValueMarks{Path{IndexStep{Key: NumberIntVal(0)}}, NewValueMarks("a")},
+			PathValueMarks{Path{IndexStep{Key: NumberIntVal(1)}}, NewValueMarks("a")},
+			false,
+		},
+		{
+			PathValueMarks{Path{IndexStep{Key: NumberIntVal(0)}}, NewValueMarks("a")},
+			PathValueMarks{Path{IndexStep{Key: NumberIntVal(0)}}, NewValueMarks("b")},
+			false,
+		},
+		{
+			PathValueMarks{Path{IndexStep{Key: NumberIntVal(0)}}, NewValueMarks("a")},
+			PathValueMarks{Path{IndexStep{Key: NumberIntVal(1)}}, NewValueMarks("b")},
+			false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("Comparing %#v to %#v", test.original, test.compare), func(t *testing.T) {
+			got := test.original.Equal(test.compare)
+			if got != test.want {
+				t.Errorf("wrong result\ngot: %v\nwant: %v", got, test.want)
+			}
+		})
+	}
+}
+
+func TestMarks(t *testing.T) {
+	wantMarks := func(marks ValueMarks, expected ...string) {
+		if len(marks) != len(expected) {
+			t.Fatalf("wrong marks: %#v", marks)
+		}
+		for _, mark := range expected {
+			if _, ok := marks[mark]; !ok {
+				t.Fatalf("missing mark %q: %#v", mark, marks)
+			}
+		}
+	}
+
+	// Single mark
+	val := StringVal("foo").Mark("a")
+	wantMarks(val.Marks(), "a")
+	val, marks := val.Unmark()
+	if val.IsMarked() {
+		t.Fatalf("still marked after unmark: %#v", marks)
+	}
+	wantMarks(marks, "a")
+
+	// Multiple marks
+	val = val.WithMarks(NewValueMarks("a", "b", "c"))
+	wantMarks(val.Marks(), "a", "b", "c")
+	val, marks = val.Unmark()
+	if val.IsMarked() {
+		t.Fatalf("still marked after unmark: %#v", marks)
+	}
+	wantMarks(marks, "a", "b", "c")
+}
+
+func TestUnmarkDeep(t *testing.T) {
+	testCases := map[string]struct {
+		val   Value
+		want  Value
+		marks ValueMarks
+	}{
+		"unmarked string": {
+			StringVal("a"),
+			StringVal("a"),
+			NewValueMarks(),
+		},
+		"marked number": {
+			NumberIntVal(1).Mark("a"),
+			NumberIntVal(1),
+			NewValueMarks("a"),
+		},
+		"unmarked list": {
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			NewValueMarks(),
+		},
+		"list with some elements marked": {
+			ListVal([]Value{NumberIntVal(1).Mark("a"), NumberIntVal(2)}),
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			NewValueMarks("a"),
+		},
+		"marked list with all elements marked": {
+			ListVal([]Value{NumberIntVal(1).Mark("a"), NumberIntVal(2).Mark("b")}).Mark("c"),
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			NewValueMarks("a", "b", "c"),
+		},
+		"marked empty list": {
+			ListValEmpty(String).Mark("c"),
+			ListValEmpty(String),
+			NewValueMarks("c"),
+		},
+		"map with elements marked": {
+			MapVal(map[string]Value{"a": StringVal("b").Mark("c"), "x": StringVal("y").Mark("z")}),
+			MapVal(map[string]Value{"a": StringVal("b"), "x": StringVal("y")}),
+			NewValueMarks("c", "z"),
+		},
+		"tuple with elements marked": {
+			TupleVal([]Value{NumberIntVal(1).Mark("a"), StringVal("y").Mark("z")}),
+			TupleVal([]Value{NumberIntVal(1), StringVal("y")}),
+			NewValueMarks("a", "z"),
+		},
+		"set with elements marked": {
+			SetVal([]Value{NumberIntVal(1).Mark("a"), NumberIntVal(2).Mark("z")}),
+			SetVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			NewValueMarks("a", "z"),
+		},
+		"complex marked object with lots of marks": {
+			ObjectVal(map[string]Value{
+				"x": ListVal([]Value{
+					NumberIntVal(3).Mark("a"),
+					NumberIntVal(5).Mark("b"),
+				}).WithMarks(NewValueMarks("c", "d")),
+				"y": StringVal("y").Mark("e"),
+				"z": BoolVal(true).Mark("f"),
+			}).Mark("g"),
+			ObjectVal(map[string]Value{
+				"x": ListVal([]Value{
+					NumberIntVal(3),
+					NumberIntVal(5),
+				}),
+				"y": StringVal("y"),
+				"z": BoolVal(true),
+			}),
+			NewValueMarks("a", "b", "c", "d", "e", "f", "g"),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got, marks := tc.val.UnmarkDeep()
+			if !got.RawEquals(tc.want) {
+				t.Errorf("wrong value\n got: %#v\nwant: %#v", got, tc.want)
+			}
+			if !marks.Equal(tc.marks) {
+				t.Errorf("wrong marks\n got: %#v\nwant: %#v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestPathValueMarks(t *testing.T) {
+	testCases := map[string]struct {
+		marked   Value
+		unmarked Value
+		pvms     []PathValueMarks
+	}{
+		"unmarked string": {
+			StringVal("a"),
+			StringVal("a"),
+			nil,
+		},
+		"marked number": {
+			NumberIntVal(1).Mark("a"),
+			NumberIntVal(1),
+			[]PathValueMarks{
+				{Path{}, NewValueMarks("a")},
+			},
+		},
+		"list with some elements marked": {
+			ListVal([]Value{NumberIntVal(1).Mark("a"), NumberIntVal(2)}),
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			[]PathValueMarks{
+				{IndexIntPath(0), NewValueMarks("a")},
+			},
+		},
+		"marked list with all elements marked": {
+			ListVal([]Value{NumberIntVal(1).Mark("a"), NumberIntVal(2).Mark("b")}).Mark("c"),
+			ListVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			[]PathValueMarks{
+				{Path{}, NewValueMarks("c")},
+				{IndexIntPath(0), NewValueMarks("a")},
+				{IndexIntPath(1), NewValueMarks("b")},
+			},
+		},
+		"marked empty list": {
+			ListValEmpty(String).Mark("c"),
+			ListValEmpty(String),
+			[]PathValueMarks{
+				{Path{}, NewValueMarks("c")},
+			},
+		},
+		"map with elements marked": {
+			MapVal(map[string]Value{"a": StringVal("b").Mark("c"), "x": StringVal("y").Mark("z")}),
+			MapVal(map[string]Value{"a": StringVal("b"), "x": StringVal("y")}),
+			[]PathValueMarks{
+				{IndexStringPath("a"), NewValueMarks("c")},
+				{IndexStringPath("x"), NewValueMarks("z")},
+			},
+		},
+		"tuple with elements marked": {
+			TupleVal([]Value{NumberIntVal(1).Mark("a"), StringVal("y").Mark("z"), ObjectVal(map[string]Value{"x": True}).Mark("o")}),
+			TupleVal([]Value{NumberIntVal(1), StringVal("y"), ObjectVal(map[string]Value{"x": True})}),
+			[]PathValueMarks{
+				{IndexIntPath(0), NewValueMarks("a")},
+				{IndexIntPath(1), NewValueMarks("z")},
+				{IndexIntPath(2), NewValueMarks("o")},
+			},
+		},
+		"set with elements marked": {
+			SetVal([]Value{NumberIntVal(1).Mark("a"), NumberIntVal(2).Mark("z")}),
+			SetVal([]Value{NumberIntVal(1), NumberIntVal(2)}),
+			[]PathValueMarks{
+				{Path{}, NewValueMarks("a", "z")},
+			},
+		},
+		"complex marked object with lots of marks": {
+			ObjectVal(map[string]Value{
+				"x": ListVal([]Value{
+					NumberIntVal(3).Mark("a"),
+					NumberIntVal(5).Mark("b"),
+				}).WithMarks(NewValueMarks("c", "d")),
+				"y": StringVal("y").Mark("e"),
+				"z": BoolVal(true).Mark("f"),
+			}).Mark("g"),
+			ObjectVal(map[string]Value{
+				"x": ListVal([]Value{
+					NumberIntVal(3),
+					NumberIntVal(5),
+				}),
+				"y": StringVal("y"),
+				"z": BoolVal(true),
+			}),
+			[]PathValueMarks{
+				{Path{}, NewValueMarks("g")},
+				{GetAttrPath("x"), NewValueMarks("c", "d")},
+				{GetAttrPath("x").IndexInt(0), NewValueMarks("a")},
+				{GetAttrPath("x").IndexInt(1), NewValueMarks("b")},
+				{GetAttrPath("y"), NewValueMarks("e")},
+				{GetAttrPath("z"), NewValueMarks("f")},
+			},
+		},
+		"path array reuse regression test": {
+			ObjectVal(map[string]Value{
+				"environment": ListVal([]Value{
+					ObjectVal(map[string]Value{
+						"variables": MapVal(map[string]Value{
+							"bar": StringVal("secret").Mark("sensitive"),
+							"foo": StringVal("secret").Mark("sensitive"),
+						}),
+					}),
+				}),
+			}),
+			ObjectVal(map[string]Value{
+				"environment": ListVal([]Value{
+					ObjectVal(map[string]Value{
+						"variables": MapVal(map[string]Value{
+							"bar": StringVal("secret"),
+							"foo": StringVal("secret"),
+						}),
+					}),
+				}),
+			}),
+			[]PathValueMarks{
+				{GetAttrPath("environment").IndexInt(0).GetAttr("variables").IndexString("bar"), NewValueMarks("sensitive")},
+				{GetAttrPath("environment").IndexInt(0).GetAttr("variables").IndexString("foo"), NewValueMarks("sensitive")},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(fmt.Sprintf("unmark: %s", name), func(t *testing.T) {
+			got, pvms := tc.marked.UnmarkDeepWithPaths()
+			if !got.RawEquals(tc.unmarked) {
+				t.Errorf("wrong value\n got: %#v\nwant: %#v", got, tc.unmarked)
+			}
+
+			if len(pvms) != len(tc.pvms) {
+				t.Errorf("wrong length\n got: %d\nwant: %d", len(pvms), len(tc.pvms))
+			}
+
+		findPvm:
+			for _, wantPvm := range tc.pvms {
+				for _, gotPvm := range pvms {
+					if gotPvm.Path.Equals(wantPvm.Path) && gotPvm.Marks.Equal(wantPvm.Marks) {
+						continue findPvm
+					}
+				}
+				t.Errorf("missing %#v\nnot found in: %#v", wantPvm, pvms)
+			}
+		})
+
+		t.Run(fmt.Sprintf("mark: %s", name), func(t *testing.T) {
+			got := tc.unmarked.MarkWithPaths(tc.pvms)
+			if !got.RawEquals(tc.marked) {
+				t.Errorf("wrong value\n got: %#v\nwant: %#v", got, tc.marked)
+			}
+		})
 	}
 }
